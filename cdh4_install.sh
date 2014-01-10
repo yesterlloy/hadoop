60f22ebda0d52be5d35316d7eb08e0363eae82d7
yongjian <76436092@qq.com>
2014-01-10 09:46:37 +0800

    Create cdh4_install.sh
    #安装程序
    #用于安装hadoop-namenode1

---
+++ cdh4_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#====================================================
+#安装程序
+#用于安装hadoop-namenode1
+#====================================================
+#@author:yangyongjian<yangyongjian@starschina.com>
+#@version:0.1
+#@date:2014-01-06
+
+#echo '安装hadoop..................'
+#source namenode_script/hadoop_install.sh
+#hadoop_install
+
+#echo '配置crontab..................'
+#source crontab_script/crontab_function.sh
+#up_crontab dopool "/root/auto_install/cron.d/dopool"
+#up_crontab hdfs "/root/auto_install/cron.d/hdfs"
+#echo '配置crontab结束.........................'
+
+#从160复制文件
+#source namenode_script/rsync_files_160.sh
